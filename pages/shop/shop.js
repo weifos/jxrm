a5892f5ccf9292168d632e66c9d3c266b317e607
叶委 <ayewei@sina.cn>
2019-07-12 11:22:14 +0800

    店铺详情JS更新

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -12,48 +12,48 @@ Component({
    */
   data: {
     shopInfo: {
+      id: 0,
+      name: '-',
+      imgurl: '../../images/index/i1.jpg',
+      business_scope: '-'
+    },
+    products: [{
       id: 1,
-      name: '我的小菜铺',
+      name: '小蘑菇',
       img: '../../images/index/i1.jpg',
-      desc: '经营山货，小食等等经营山货，小食等等经营山货，小食等等经营山货，小食等等',
-      products: [{
-          id: 1,
-          name: '小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        },
-        {
-          id: 111,
-          name: '小蘑菇小蘑菇小蘑菇小蘑菇小蘑菇小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        },
-        {
-          id: 111,
-          name: '小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        },
-        {
-          id: 111,
-          name: '小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        },
-        {
-          id: 111,
-          name: '小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        },
-        {
-          id: 111,
-          name: '小蘑菇',
-          img: '../../images/index/i1.jpg',
-          price: 12
-        }
-      ]
+      price: 12
+    },
+    {
+      id: 111,
+      name: '小蘑菇小蘑菇小蘑菇小蘑菇小蘑菇小蘑菇',
+      img: '../../images/index/i1.jpg',
+      price: 12
+    },
+    {
+      id: 111,
+      name: '小蘑菇',
+      img: '../../images/index/i1.jpg',
+      price: 12
+    },
+    {
+      id: 111,
+      name: '小蘑菇',
+      img: '../../images/index/i1.jpg',
+      price: 12
+    },
+    {
+      id: 111,
+      name: '小蘑菇',
+      img: '../../images/index/i1.jpg',
+      price: 12
+    },
+    {
+      id: 111,
+      name: '小蘑菇',
+      img: '../../images/index/i1.jpg',
+      price: 12
     }
+    ]
   },
 
   /**
@@ -78,9 +78,9 @@ Component({
         url: '../home/productDetail/productDetail?id=' + id
       })
     },
-    api_201: function() {
+    api_202: function() {
       var this_ = this;
-      wx.post(api.api_201, wx.GetSign(), function(app, res) {
+      wx.post(api.api_202, wx.GetSign(), function(app, res) {
         if (res.data.Basis.State != api.state.state_200) {
           wx.showToast({
             title: res.data.Basis.Msg,
@@ -90,9 +90,6 @@ Component({
         } else {
           this_.setData({
             banners: res.data.Result.banners
-          })
-          this_.setData({
-            cat: res.data.Result.stores
           })
         }
       });
